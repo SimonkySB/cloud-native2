b361e51bac463e07806c91bad9aa31e9e275d4fa
simonky <simonasb19@gmail.com>
2025-03-26 09:05:02 -0300

    Functions

---
+++ functions/src/main/java/com/cloudnative/models/Rol.java
@@ -0,0 +1,39 @@
+package com.cloudnative.models;
+
+
+
+
+public class Rol {
+    
+    private Long id;
+    private String name;
+
+    
+    public Rol() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    
+
+    
+}

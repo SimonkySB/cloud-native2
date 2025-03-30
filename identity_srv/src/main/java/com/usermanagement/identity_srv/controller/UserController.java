9fad0efdee8c8b7911082de299d5c73bf818b288
gustavoromerocl <gandresrp@gmail.com>
2025-03-30 11:23:57 -0300

    feat: create user service

--- identity_srv/src/main/java/com/usermanagement/identity_srv/controller/UserController.java
+++ identity_srv/src/main/java/com/usermanagement/identity_srv/controller/UserController.java
@@ -2,12 +2,19 @@ package com.usermanagement.identity_srv.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.usermanagement.identity_srv.dto.UserCreateRequest;
 import com.usermanagement.identity_srv.model.User;
 import com.usermanagement.identity_srv.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,4 +30,11 @@ public class UserController {
   public List<User> getAllUsers() {
     return service.getAllUsers();
   }
+
+  @PostMapping
+  public ResponseEntity<User> createUser(@Valid @RequestBody UserCreateRequest request) {
+    User newUser = service.createUser(request);
+    return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+  }
+
 }
--- identity_srv/src/main/java/com/usermanagement/identity_srv/service/UserService.java
+++ identity_srv/src/main/java/com/usermanagement/identity_srv/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.usermanagement.identity_srv.dto.UserCreateRequest;
 import com.usermanagement.identity_srv.model.User;
 import com.usermanagement.identity_srv.repository.UserRepository;
 
@@ -19,4 +20,13 @@ public class UserService {
   public List<User> getAllUsers() {
     return repository.findAll();
   }
+
+  public User createUser(UserCreateRequest request) {
+    User user = new User();
+    user.setEmail(request.getEmail());
+    user.setUsername(request.getUsername());
+    user.setPassword(request.getPassword()); // ⚠️ Esto luego se debe hashear
+    return repository.save(user);
+  }
+
 }

aa389601d050bc495b07512542b2a2da7ad0e28d
simonky <simonasb19@gmail.com>
2025-03-30 12:17:12 -0300

    refactor: az functions

---
+++ functions/src/main/java/com/cloudnative/FuncNotificacionActividadSospechoza.java
@@ -0,0 +1,45 @@
+package com.cloudnative;
+
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+public class FuncNotificacionActividadSospechoza {
+
+    @FunctionName("FuncNotificacionActividadSospechoza")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req", 
+                route = "FuncNotificacionActividadSospechoza",
+                methods = {HttpMethod.POST}, 
+                authLevel = AuthorizationLevel.ANONYMOUS) 
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+
+        context.getLogger().info("FuncNotificacionActividadSospechoza, Java HTTP trigger processed a request.");
+
+        
+        String body = request.getBody().orElse("");
+                
+        try {
+            if(body.isEmpty()) {
+                return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Cuerpo de la solicitud vacio").build();
+            }    
+
+            return request.createResponseBuilder(HttpStatus.OK).body("Se ha detectado una actividad sospechoza e informado al usuario: " + body).build();
+
+        } catch (Exception ex) {
+            context.getLogger().severe("FuncNotificacionActividadSospechoza, Error procesando solicitud");
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + body).build();
+        }
+
+        
+    }
+}

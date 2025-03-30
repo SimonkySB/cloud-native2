d65c50330035f4859d625bf31c075a387e04d56d
gustavoromerocl <gandresrp@gmail.com>
2025-03-30 00:27:38 -0300

    feat: implement identity base project

---
+++ identity_srv/src/main/java/com/usermanagement/identity_srv/IdentitySrvApplication.java
@@ -0,0 +1,13 @@
+package com.usermanagement.identity_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentitySrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentitySrvApplication.class, args);
+	}
+
+}

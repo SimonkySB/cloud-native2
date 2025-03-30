2f96aaffff3fffc61a3acb3aca3530c6821b81d0
gustavoromerocl <gandresrp@gmail.com>
2025-03-30 15:55:53 -0300

    feat: create permissions base project

---
+++ permissions_srv/src/main/java/com/usermanagement/permissions_srv/PermissionsSrvApplication.java
@@ -0,0 +1,13 @@
+package com.usermanagement.permissions_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionsSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionsSrvApplication.class, args);
+	}
+
+}
